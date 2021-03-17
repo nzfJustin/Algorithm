24b59c45a563880d577358f19b2678b5dd540f0d
Justin Nie <43760617+nzfJustin@users.noreply.github.com>
2021-03-17 16:33:06 -0700

    Update numPath.java

--- numPath.java
+++ numPath.java
@@ -20,15 +20,6 @@ both have time complexity of O(m*n). space complexity of O(m*n)
 if you could think of any optimization, please let me know. Thank you!
 
 class numPath{
-    /*
-    给一个 M*N 矩阵，每一点只能往右上、右、右下走，问从左下角(A)走到右下角(B)共 有几种走法。
-    加强版:
-    现在有另一个参数 H，如果从 A 到 B 之间 path 的高度不小于 H 才算是 valid path，问 共有几种走法。
-    比如 M = 3，N = 4，H = 1 就有如下三种走法 (1 代表走的路): 
-    0000 0000 0000
-    0010 0100 0110
-    1101 1011 1001
-    */
     public static int numOfWaysHard(int[][] matrix, int h) {
         int m = matrix.length;
         int n = matrix[0].length;
@@ -70,7 +61,8 @@ class numPath{
     // each cell's value represent the # of ways to reach this cell from bottom left corner 
     // ways here is used to store the previous cell's value
     // so that we can update current cell's value based on previous cell's value (just increment by 1)
-    public static void dfs(int[][]matrix, int i, int j, List<Integer>list, List<List<Integer>>res, int m, int n, int small, int large, int height) {
+    
+   public static void dfs(int[][]matrix, int i, int j, List<Integer>list, List<List<Integer>>res, int m, int n, int small, int large, int height) {
         if(i==m && j==n && large-small >= height*m) {
             //System.out.println(small+","+large);
             res.add(new ArrayList<Integer>(list));
